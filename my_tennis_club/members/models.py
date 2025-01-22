89f979485460f3c80b7ad48f82d639c0a699e774
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2025-01-22 12:12:51 -0300

    se agregan páginas y se trabaja con master

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
